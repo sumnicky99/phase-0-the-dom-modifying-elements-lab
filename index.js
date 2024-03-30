b63b1ef95ff69a8186f7ca59329790cc4d9a4873
Sumnicky99 <sumnicky99@gmail.com>
2024-03-30 12:46:03 +0300

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code here!
+let main = document.getElementById("main");
+main.remove();
+
+let newHeader = document.createElement("h1");  newHeader.id = "victory";
+newHeader.innerHTML = "Baklawa is the champion";
